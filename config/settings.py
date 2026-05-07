86f8a5e6a433386f153328211134b3c48fcce842
Mohamed Brahimi <momohb2007@gmail.com>
2026-05-07 14:19:05 -0400

    Fix Render static files

--- config/settings.py
+++ config/settings.py
@@ -121,6 +121,8 @@ USE_TZ = True
 # Here, they well be accessible at your-domain.onrender.com/static/... or yourcustomdomain.com/static/...
 STATIC_URL = '/static/'
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 # This production code might break development mode, so we check whether we're in DEBUG mode
 if not DEBUG:
     # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
